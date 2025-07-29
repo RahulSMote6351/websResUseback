6a17937e0ccc7d3ed697d0ea9e357f5665462ae3
RahulSMote6351 <rmote6351@gmail.com>
2025-07-29 19:03:41 +0530

    add something

--- models/user.js
+++ models/user.js
@@ -11,6 +11,10 @@ const userSchema =  new mongoose.Schema({
     message:{
         type:String,
         required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now()
     }
 });
 
